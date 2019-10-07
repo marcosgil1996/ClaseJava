647e6052b80ac03df8895e3163cede8c77cbc636
SergiSvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-07 10:12:34 +0200

    Act.11 Resta

---
+++ Tema 3/Act.14_Circulos.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Circulos {
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        
+        // Declaro las variables ncesarias
+        double radio;
+        double longitud;
+        double area;
+        final float PI = 3.14159265359f;
+        
+        //pantalla
+        System.out.println("Ingresar el radio de la circunferencia");
+        radio = lector.nextDouble();
+        lector.nextInt();
+        
+        //Calculos
+        PI = 4;
+        longitud = 2*PI*radio;
+        area = radio*radio;
+        
+
+
+
+    }
+
+}
