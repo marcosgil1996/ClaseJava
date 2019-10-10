df192e7ae9262e8dd6d7d0ad420b86128c14e03b
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-10 17:47:37 +0200

    Correcgido

--- Tema 4/Act.1Par.java
+++ Tema 4/Act.1Par.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Par {
+public class Act1Par {
 
     public static void main(String[] args) {
         
@@ -8,7 +8,7 @@ public class Par {
         Scanner teclado = new Scanner(System.in); //Lectura de Scaner
         
         System.out.printf("Introduce el número que quieres averiguar si es par o impar"); //Se pide el dato al usuario
-        numero = teclado.nextLine(); // La clase String no puede convertirse en un int ?
+        numero = teclado.nextInt(); // La clase String no puede convertirse en un int ?
         teclado.nextLine();
 
         //Si el numero introduccido es par, es decir, divisible por 2 el resto será igual a 0.  Por tanto;
