95d01d0bef7314c7ceda70f90e3e4e9ed2554d0c
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-15 18:32:58 +0200

    Update Act8Contador.java

--- Tema 4/Act8Contador.java
+++ Tema 4/Act8Contador.java
@@ -1,13 +1,16 @@
 import java.io.*;
 
+/**
+ * Un programa que cuente de 45 a 100
+ */
+
 public class Act8Contador{
 
 	public static void main(String args[]) {
-		int contador;
+        int contador;
+        
 		for (contador=45;contador<=100;contador+=1) {
 			System.out.println(contador);
-		}
+        }
 	}
-
-
 }
