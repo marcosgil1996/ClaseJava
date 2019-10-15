1e7632389c512779e8fd1d1519ab9674586c55e8
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-15 18:22:35 +0200

    Create Act8Contador.java

---
+++ Tema 4/Act8Contador.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Act8Contador{
+
+	public static void main(String args[]) {
+		int contador;
+		for (contador=45;contador<=100;contador+=1) {
+			System.out.println(contador);
+		}
+	}
+
+
+}
