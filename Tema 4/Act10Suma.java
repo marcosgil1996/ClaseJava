1f19ff1b449e432b8be41724627bc2bbc4ca4257
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-15 19:19:30 +0200

    Create Act10Suma.java

---
+++ Tema 4/Act10Suma.java
@@ -0,0 +1,17 @@
+import java.Scanner;
+ 
+/** La Multiplicación de dos numeros usando Sumas */
+
+public class Act10Suma{
+    
+     public static void main(String[] args) {
+        int numero1, numero2; //Variables
+        Scanner teclado = new Scanner(System.in); //Lectura de Scaner
+            
+        System.out.printf("Introduce introduce el primer numero que quieres que multiplique"); //Se pide el dato al usuario
+        numero = teclado.nextInt(); // La clase String no puede convertirse en un int ?
+        teclado.nextLine();
+        
+    }
+
+}
