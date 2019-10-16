e26e326c3f7b39cdf00de768cb6d6ebd4be2287a
SergiSvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-16 11:17:26 +0200

    Update Act10Suma.java

--- Tema 4/Act10Suma.java
+++ Tema 4/Act10Suma.java
@@ -1,17 +1,30 @@
-import java.Scanner;
+import java.util.Scanner;
  
-/** La Multiplicación de dos numeros usando Sumas */
+/** 
+ * La Multiplicación de dos numeros usando Sumas 
+ */
 
 public class Act10Suma{
     
      public static void main(String[] args) {
-        int numero1, numero2; //Variables
+        int numero1, numero2, producto = 0; //Variables
         Scanner teclado = new Scanner(System.in); //Lectura de Scaner
             
-        System.out.printf("Introduce introduce el primer numero que quieres que multiplique"); //Se pide el dato al usuario
-        numero = teclado.nextInt(); // La clase String no puede convertirse en un int ?
+        System.out.println("Introduce introduce el primer número que quieres que multiplique"); //Se pide el primer dato al usuario
+        numero1 = teclado.nextInt(); // Guardamos el valor del numero
         teclado.nextLine();
-        
+
+        System.out.println("Introduce introduce el segundo número que quieres que multiplique"); //Se pide el segundo dato al usuario
+        numero2 = teclado.nextInt(); // Guardamos el valor del numero
+        teclado.nextLine();
+
+        while( numero2 !=0 )  {
+            producto = producto + numero1;
+            numero2 = numero2 - 1;
+        }
+
+        System.out.println("El producto entre los números introducidos es: " + producto );
+        teclado.close(); // Cierre del teclado
     }
 
 }
