265511c69f811ba6342e6c6603298c2df7d45d58
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-18 18:45:30 +0200

    Create Act06MonedaC.java

---
+++ Tema 4/Act06MonedaC.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//** Incompleto **/
+
+public class Act06MonedaC {
+     public static void main(String[] args) {
+         Scanner teclado = new Scanner (System.in);
+
+         float cantidad, moneda2, moneda1, moneda50C, moneda20C, moneda10C, moneda5C, moneda2C, moneda1C;
+
+         System.out.println("Por favor, escribe la cantidad de dinero: "); // Se pide al usuario el dinero
+         cantidad = teclado.nextFloat();
+
+         cantidad = cantidad *100;
+         moneda2 = (int)cantidad / 100;
+         cantidad = cantidad %200;
+         moneda1 = (int)cantidad /100;
+         cantidad = cantidad %100;
+         moneda50C = (int)cantidad /50;
+         cantidad = cantidad %50;
+         moneda20C = (int)cantidad /20;
+         cantidad = cantidad % 20;
+
+
+        
+    }
+}
