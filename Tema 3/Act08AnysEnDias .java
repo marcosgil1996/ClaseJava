fe771f63f4d625259154de82a39305dadadd8c37
SergiSvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-20 10:54:18 +0200

    Create Act08AnysEnDias .java

---
+++ Tema 3/Act08AnysEnDias .java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Act08AnysEnDias{
+
+    public static void main(String args[]){
+        final int DIAS_ANYO = 365;
+        Scanner scan = new Scanner(Systeam.in);
+        int edad, dias;
+
+        System.out.println("* Por favor, indeque su edad; " );
+
+        edad = scan.nextInt;
+        dias = edad * DIAS_ANYO;
+        System.out.println("Usted tiene " + dias + " dias de vida" );
+
+    }
+}
