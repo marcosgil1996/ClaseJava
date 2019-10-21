c211dcbee99d5273874b31c6d7a63db122c90886
SergiSvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-21 10:10:00 +0200

    Update Act19Adivina.java

---
+++ Tema 4/Act19Adivina.java
@@ -0,0 +1,16 @@
+/*
+* Escribe un programa que permita determinar si utilizar los números aleatorios de la clase Random
+* son apropiados. El programa tiene que simular que se lanza una moneda al aire 1.000.000 de
+veces. A continuación debe imprimir por pantalla el porcentaje de caras y el porcentaje de cruces
+que han salido.
+*/
+
+import java.util.match;
+import java.util.scanner;
+
+public class Act19Adivina{
+    public static void main(String[] args) {
+        Scanner teclado = new scanner(System.in);
+
+    }
+}
