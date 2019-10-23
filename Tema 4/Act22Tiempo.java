4a1d2a39da9c8b064a811ec2a06d7b3832d1dc4c
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-23 16:38:11 +0200

    Update Act22Tiempo.java

--- Tema 4/Act22Tiempo.java
+++ Tema 4/Act22Tiempo.java
@@ -1,29 +1,54 @@
-import java.util.Scanner;
+import java.util.Scanner;//Importamos el escáner.
 
-public class Act22Tiempo {
-	public static void main(String arg[]) {
-        int hour;   // variables que vamos a necesitar
-        Scanner teclado = new Scanner (System.in); //Scaner lectura
-        boolean valido;
+
+public class Act22{
+    public static void main(String[] args) {
+        int horas;//Variable que almacena las horas introducidas.
+        int minutos;//Variable que almacena los minutos introducidos.
+        int segundos;//Variable que almacena los segundos.
+        boolean horasOK = false;//Variable booleana que almacenará si el número está bien o no.
+        boolean minutosOK = false;//Variable booleana que almacenará si el número está bien o no.
+        boolean segundosOK = false;//Variable booleana que almacenará si el número está bien o no.
+        Scanner lector = new Scanner (System.in);//Declaramos el escáner.
+
+
+        System.out.println("Introduce el número de segundos (0-60): ");//Pedimos por primera vez los segundos.
+        segundos = Integer.parseInt(lector.nextLine());//Leemos los segundos.
+        System.out.println("Introduce el número de minutos (0-60): ");//Pedimos por primera vez los minutos.
+        minutos = Integer.parseInt(lector.nextLine());//Leemos los minutos.
+        System.out.println("Introduce el número de horas (0-24): ");//Pedimos por primera vez las horas.
+        horas = Integer.parseInt(lector.nextLine());//Leemos las horas.
+
+
+        //Hacemos un bucle while, mientras que horasOK, minutosOK o segundosOK sean false el bucle se
+        //seguirá haciendo, aunque gracias al condicional no volverá a pedir la fecha si ya se ha 
+        //introducido correctamennte anteriormente.
         
-        
-        do{
-            System.out.print( "Introduce una hora" ); // número insertado en horas hasta un máx de 24h
-            teclado.nextLine();
-            hour = teclado.nextInt(); //numero introducido
-            valido = hour<=23;
-            
-            
-            if(!valido){
-                System.out.print("Establezca una hora correcta");
-                system.print.nextLine();
-            }else(valido){
-                System.out.print("hora correcta");
-                system.print.nextLine();
+        while (!horasOK || !minutosOK || !segundosOK){
+            if (segundos > 60 || segundos < 0){
+                System.out.println("Te has equivocado! Introduce el número de segundos (0-60): ");
+                segundos = Integer.parseInt(lector.nextLine());
+            }else{
+                segundosOK = true;
             }
-        }while (valido);
+        
+            if (minutos > 60 || minutos < 0){
+                System.out.println("Te has equivocado! Introduce el número de minutos (0-60): ");
+                minutos = Integer.parseInt(lector.nextLine());
+            }else {
+                minutosOK = true;
+            }
+            if (horas > 24 || horas < 0){
+                System.out.println("Te has equivocado! Introduce el número de horas (0-24): ");
+                horas = Integer.parseInt(lector.nextLine());
+            }else{
+                horasOK = true;
+            }
 
-		System.out.println(" El valor introducido como " + hour+ " es correcto");
-		teclado.close();
-	}
+        }
+        //Sacamos por pantalla los resultados y cerramos el lector.
+        System.out.println("La fecha correcta que has introducido es: " + horas + " horas, " + minutos + " minutos y " + segundos + " segundos"); 
+        lector.close();
+
+    }
 }
