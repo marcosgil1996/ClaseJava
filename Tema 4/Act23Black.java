d237559566b1ddac89b1f42e14f6d4ff9af3afb3
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-24 17:58:01 +0200

    Update Act23Black.java

--- Tema 4/Act23Black.java
+++ Tema 4/Act23Black.java
@@ -12,18 +12,28 @@ import java.io.*; //Importamos la clase IO
         Scanner teclado = new Scanner(system.in); // Declaro el scanner
         Random rd = new Random(system.in); // Declaro el radom
 
-        int cara1, cara2, cara3, cara4, cara5, cara6; //caras del dado
         int player1, player2; //En este caso el jugador uno será el usuario y el jugador dos sera la máquina
         int dado; //dado
+        int sumaDado = 0;
+        int porras = 0;
+        int totalDados=0;
+        int totakDadosCuoier =0 ;
+        int porrasCupier =0;
         int win = 22; //si llega a 22 ganas
-        String seguir = "x"; // clase char para que el jugador pueda avanzar en el juego
+        String seguir = "S"; // clase char para que el jugador pueda avanzar en el juego
+        String parar ="N"; //El jugador no avanza
+
 
         System.out.println("========BLACK JACK========");
         System.out.println("===BIENVENIDO AL JUEGO===");
 
-        while("y".equals(seguir)){
-            dado = rd.nextInt(10);
+        while(porras <5 && porrasCupier<5){
+            sumaDado =0;
+            totalDados=0;
+            totalDadosCupier = false;
+            System.out.println("***");
 
+            
         }
 
      }
