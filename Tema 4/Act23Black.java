29c089eb316ff02fed8ffc2f4501651591d31150
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-24 17:27:04 +0200

    Update Act23Black.java

--- Tema 4/Act23Black.java
+++ Tema 4/Act23Black.java
@@ -1,6 +1,6 @@
-import java.util.Random;
-import java.util.Scanner;
-import java.io.*;
+import java.util.Random; // Importamos la clase Radom
+import java.util.Scanner; //Importamos la clase Scanner
+import java.io.*; //Importamos la clase IO
 
 /** 
  * Black Jack
@@ -16,89 +16,16 @@ import java.io.*;
         int player1, player2; //En este caso el jugador uno será el usuario y el jugador dos sera la máquina
         int dado; //dado
         int win = 22; //si llega a 22 ganas
-        char seguir = x; // clase char para que el jugador pueda avanzar en el juego
+        String seguir = "x"; // clase char para que el jugador pueda avanzar en el juego
 
         System.out.println("========BLACK JACK========");
         System.out.println("===BIENVENIDO AL JUEGO===");
 
+        while("y".equals(seguir)){
+            dado = rd.nextInt(10);
+
+        }
+
      }
  }
-
- import java.util.Scanner;
-import java.util.*;
  
-public class Blackjack {
-    public static void main(String[] args) {
- 
-        String anotherCard, playAgain = "y", ctn = null;
-        int nextCard, card1, card2, dCard1, dCard2;
-        int cardTotal = 0, dTotal = 0;
- 
-        Scanner keyboard = new Scanner(System.in);
- 
-        Random random = new Random();
- 
-        // Begin dealing the players first two cards
- 
-        while ("y".equals(playAgain)) {
-            // dealers first two random cards
-            dCard1 = random.nextInt(10) + 1;
-            dCard2 = random.nextInt(10) + 1;
- 
-            // players first two random cards and card total
-            card1 = random.nextInt(10) + 1;
-            card2 = random.nextInt(10) + 1;
-            cardTotal = card1 + card2;
- 
-            // dealers two card total and display only one dealer card
-            dTotal = dCard1 + dCard2;
-            System.out.println("Dealer shows: " + dCard1);
- 
-            // display players first two cards & card total
-            System.out.println("First Cards: " + card1 + ", " + card2);
-            System.out.println("Total: " + cardTotal);
- 
-            System.out.println("Another Card (y/n)?: ");
-            anotherCard = keyboard.nextLine();
- 
-            while ("y".equals(anotherCard)) {
-                nextCard = random.nextInt(10) + 1;
-                cardTotal += nextCard;
-                System.out.println("Card: " + nextCard);
-                System.out.println("Total: " + cardTotal);
- 
-                if (cardTotal > 21) {
-                    System.out.println("You busted, Dealer wins");
-                    System.out.println("Do you want to play again? (y/n):");
-                    playAgain = keyboard.nextLine();
-                }
- 
-                if (cardTotal < 21)
-                    System.out.println("Another Card (y/n)?: ");
-                 
-                anotherCard = keyboard.nextLine();
-                 
-                if ("n".equals(anotherCard))
-                    System.out.println("Dealer had: " + dTotal);
-                 
-                System.out.println("You had: " + cardTotal);
- 
-                while ("n".equals(anotherCard)) {
-                    if (dTotal < cardTotal && cardTotal < 21) {
-                        System.out.println("Yay you win!");
-                        System.out.println("Play again? (y/n): ");
-                        playAgain = keyboard.nextLine();
-                         
-                        if (playAgain.equalsIgnoreCase("y"))
-                            playAgain = "y";
- 
-                        if (dTotal > cardTotal && dTotal < 21)
-                            System.out.println("You lose!");
-                         
-                        playAgain = keyboard.nextLine();
-                    }
-                }
-            }
-        }
-    }
-}
