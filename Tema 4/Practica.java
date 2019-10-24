417ebae7a59c0b1eff78e25450be6af7d5dfdfe1
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-24 19:20:31 +0200

    Create Practica.java

---
+++ Tema 4/Practica.java
@@ -0,0 +1,47 @@
+import java.util.Scanner; //importamos la clase Scanner
+
+public class Practica{
+    public static void main(String[] args) {
+        //Variables que vamos a gastar
+        int opcion; //opciones del menú
+        boolean salir= false;
+        double resultado;
+        //Invocamos el scanner
+        Scanner teclado = new Scanner(system.in);
+        
+        while (!salir) {
+            
+            //Menú Principal
+            System.out.println("ELIGE UNA OPCIÓN");
+            System.out.println("1. SUMAR");
+            System.out.println("3. RESTAR");
+            System.out.println("4. MULTIPLICAR");
+            System.out.println("5. RESTO DIVISIÓN");
+            System.out.println("- - - - - - - - - - - - ");
+            System.out.println("SALIR");
+            opcion = teclado.nextInt();
+            teclado.nextLine();
+            
+            switch (opción) {
+                case 1:
+                    System.out.println("Has saleccionado la sumana: ");
+                    System.out.println("Ingrese");
+
+                    break;
+                case 2:
+                case 3:
+                
+            
+                
+            }
+        
+
+            
+        }
+
+        
+
+
+        
+    }
+}
