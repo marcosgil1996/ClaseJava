0cf336441834069c8b6980d4a622981169f43a18
SergiSvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-25 11:01:28 +0200

    Update Practica.java

--- Tema 4/Practica.java
+++ Tema 4/Practica.java
@@ -4,8 +4,10 @@ public class Practica{
     public static void main(String[] args) {
         //Variables que vamos a gastar
         int opcion; //opciones del menú
+        int primerNumero, segundoNumero; //los valores que dará el usuario cuan vaya ha realizar las operaciones
         boolean salir= false;
         double resultado;
+
         //Invocamos el scanner
         Scanner teclado = new Scanner(system.in);
         
@@ -13,35 +15,77 @@ public class Practica{
             
             //Menú Principal
             System.out.println("ELIGE UNA OPCIÓN");
-            System.out.println("1. SUMAR");
-            System.out.println("3. RESTAR");
-            System.out.println("4. MULTIPLICAR");
-            System.out.println("5. RESTO DIVISIÓN");
+            System.out.println(" 1. SUMAR \n 2. RESTAR \n 3. MULTIPLICAR \n 4. DIVIDIR \n 5. RESTO DIVISIÓN");
             System.out.println("- - - - - - - - - - - - ");
-            System.out.println("SALIR");
+            System.out.println("0. SALIR");
             opcion = teclado.nextInt();
             teclado.nextLine();
             
             switch (opción) {
-                case 1:
-                    System.out.println("Has saleccionado la sumana: ");
-                    System.out.println("Ingrese");
-
+                case 0:
+                    break;
+                case 1: //Suma
+                    System.out.println("Has saleccionado la Suma: \n Introduce el primer numero numero a sumar");
+                    primerNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    System.out.println("Introduce el segundo numero numero a sumar");
+                    segundoNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    resultado = primerNumero + segundoNumero;
+                    System.out.println("El resultado es: " + resultado);
+                    resultado = 0;
                     break;
                 case 2:
+                    System.out.println("Has saleccionado Restar: \n Introduce el primer numero numero a restar");
+                    primerNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    System.out.println("Introduce el segundo numero numero a restar");
+                    segundoNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    resultado = primerNumero - segundoNumero;
+                    System.out.println("El resultado es: " + resultado);
+                    resultado = 0;
+                    break;
                 case 3:
-                
-            
+                    System.out.println("Has saleccionado la Multiplicar: \n Introduce el primer numero numero a multiplicar");
+                    primerNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    System.out.println("Introduce el segundo numero numero a multiplicar");
+                    segundoNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    resultado = primerNumero * segundoNumero;
+                    System.out.println("El resultado es: " + resultado);
+                    resultado = 0;
+                    break;
+                case 4: 
+                    System.out.println("Has saleccionado la Divisón: \n Introduce el primer numero numero a dividir");
+                    primerNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    System.out.println("Introduce el segundo numero numero a dividir");
+                    segundoNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    resultado = primerNumero / segundoNumero;
+                    System.out.println("El resultado es: " + resultado);
+                    resultado = 0;
+                    break;
+                case 5:
+                    System.out.println("Has saleccionado el Resto de la División: \n Introduce el primer numero numero a dividir");
+                    primerNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    System.out.println("Introduce el segundo numero numero a dividir");
+                    segundoNumero = teclado.nextInt();
+                    teclado.nextLine();
+                    resultado = primerNumero % segundoNumero;
+                    System.out.println("El resultado es: " + resultado);
+                    resultado = 0;
+                    break;
+                default:
+                    System.out.println("Has saleccionado un número no valido porfavor seleccione un numero de 0 a 5");
+                 break;
                 
             }
-        
 
-            
         }
-
-        
-
-
         
     }
 }
