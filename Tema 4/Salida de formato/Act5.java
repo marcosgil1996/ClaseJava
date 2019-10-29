9853de79edf421840aa3829d432f757125731bcb
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-29 19:18:16 +0100

    Update Act5.java

--- Tema 4/Salida de formato/Act5.java
+++ Tema 4/Salida de formato/Act5.java
@@ -4,6 +4,12 @@
 public class Act5 {
 
     public static void main(String[] args) {
-        
+        String s ="";
+        for(int i =1; i<=9;i++){
+            s = s + i;
+            System.out.printf("\u001B[0;30;43m %9s \n", s);
+            System.out.println("\u001B[0;30;43m \u001B[0m");
+
+        }
     }
 }
