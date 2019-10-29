517bb3a5b87e8b03632b04c8f09027a60adf10de
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-29 17:44:15 +0100

    Create Act4.java

---
+++ Tema 4/Salida de formato/Act4.java
@@ -0,0 +1,15 @@
+/**
+ * Act4
+ */
+public class Act4 {
+
+    public static void main(String[] args) {
+        String s ="";
+       
+        for(int i =1; i<=9;i++){
+            s = s + i;
+            System.out.printf("%-9s \n", s);
+
+        }
+    }
+}
