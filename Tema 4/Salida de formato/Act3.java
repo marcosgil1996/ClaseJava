2675845f072835226aaedb179df71c90afe300b2
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-29 17:53:07 +0100

    Update Act3.java

--- Tema 4/Salida de formato/Act3.java
+++ Tema 4/Salida de formato/Act3.java
@@ -6,7 +6,7 @@ public class Act3 {
         String s ="";
         for(int i =1; i<=9;i++){
             s = s + i;
-            System.out.printf("%9s", s);
+            System.out.printf("%9s \n", s);
 
         }
         
