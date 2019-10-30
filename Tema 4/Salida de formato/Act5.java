ff71e60d70b5656b6bc576a7666f6de83e5a92fd
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-10-30 15:47:42 +0100

    Update Act5.java

--- Tema 4/Salida de formato/Act5.java
+++ Tema 4/Salida de formato/Act5.java
@@ -5,10 +5,13 @@ public class Act5 {
 
     public static void main(String[] args) {
         String s ="";
+        int n = 0; //contador
+
         for(int i =1; i<=9;i++){
             s = s + i;
-            System.out.printf("\u001B[0;30;43m %9s \n", s);
-            System.out.println("\u001B[0;30;43m \u001B[0m");
+            System.out.printf("\u001B[0;3"+n+"m%9s \n", s);
+            System.out.println("\u001B[0;3 \u001B[0m");
+            n++;
 
         }
     }
