f115bf71c9f38d409cb91f8cfe86c1048546201e
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-05 18:02:02 +0100

    Update Act9.java

--- Tema 4/Salida de formato/Act9.java
+++ Tema 4/Salida de formato/Act9.java
@@ -1,9 +1,30 @@
 /**
- * Act9
+ * Act9 un programa para representar los datos de una estación meteorológica. 
  */
+import java.util.Scanner;
+
 public class Act9 {
 
     public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(system.in);
+        //Ciudad
+        String ciudad;
+        //Fechas
+        int hora, second, minute;
+        //Velocidad Viento
+        int viento;
+        //Temperatura
+        int temperatura;
+        //Presión en hPa
+        int presion;
+        //Probalidad de lluvia 0-100%
+        int lluvia;
+        //UVI, entero
+        int uvi;
+
+        //Vamos a pedir los datos a los usuarios//
+        
         
     }
 }
