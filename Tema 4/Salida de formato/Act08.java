3766c504557053ce4da214cc068649b43e2696f0
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-05 17:19:55 +0100

    Create Act08.java

---
+++ Tema 4/Salida de formato/Act08.java
@@ -0,0 +1,19 @@
+/**
+ * Act08 Escribe un programa que haciendo uso de bucles anidados muestre la tabla de colores ANSI de la
+ * especificación inicial
+ */
+public class Act08 {
+
+    public static void main(String[] args) {
+
+        for(int i=0; i<=i; i++){
+            for(int j = 0; j<=7;i++){
+                for(int k=0;k<=7; k++){
+                    System.out.printf(i+"3;"+j+"4;"+4);
+                }
+                
+            }
+        }
+        
+    }
+}
