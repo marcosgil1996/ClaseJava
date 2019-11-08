a645f71851cbad6fcdc889440aba10a938fc8d09
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-08 18:01:45 +0100

    Update Act2.java

--- Tema 6/Act2.java
+++ Tema 6/Act2.java
@@ -1,9 +1,19 @@
 /**
- * Act2
+ * Act2 Escribe un programa que calcule el área y la longitud de una circunferencia en función del radio
+ * (leído desde teclado). Hay que escribir un método para calcular el área y otro para la longitud.
  */
+import java.util.Scanner;
+
 public class Act2 {
 
     public static void main(String[] args) {
-        int g
+        
+        Scanner teclado = new Scanner(System.in);
+        
+        Math PI;   
+        System.out.println("Area de la circunferencia: ");
+        
+
+       teclado.close();
     }
 }
