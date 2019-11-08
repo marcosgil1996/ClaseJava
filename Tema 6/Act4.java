c4ee927778ccc41c9892afd67d05cfd3d933c454
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-08 19:13:59 +0100

    Update Act4.java

---
+++ Tema 6/Act4.java
@@ -0,0 +1,29 @@
+/**
+ * Act4 Escribe un método que devuelva el mayor de cuatro números
+ */
+public class Act4 {
+
+    public static void main(String[] args) {
+        
+    }
+
+    public static double maximo(double a, double b, double c, double d) {
+        double max1 = ma
+        
+    }
+    
+    /**
+     * 
+     * @param x
+     * @param y
+     * @return
+     */
+    public static double maximo( double x, double y) {
+        if (x>=b) {
+            return x;
+            
+        } else{
+            return y;
+        } 
+    }
+}
