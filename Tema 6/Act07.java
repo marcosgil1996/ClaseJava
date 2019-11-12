4e19a15712fd2ff039eca772eb39520fb375f494
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-12 17:59:49 +0100

    Update Act07.java

---
+++ Tema 6/Act07.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * Act07 Escribe un método que acepte un número entero y calcule su factorial
+ */
+public class Act07 {
+
+    public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        int num;
+        System.out.println("Intriduce el número del cual quieras calcualar el factorial");
+
+    }
+    public static int factorial(int a) {
+        int b = 1;
+        for(int i = a;i>)
+    }
+}
