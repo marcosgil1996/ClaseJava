9839ab19e696052df2bd1184b44e1e2521e83a02
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-12 17:25:00 +0100

    Update Act5.java

---
+++ Tema 6/Act5.java
@@ -0,0 +1,13 @@
+/**
+ * Act5 Escribe un método que acepte dos argumentos: el carácter que se desea imprimir y el número a
+*  veces que se imprime.
+ */
+public class Act5 {
+
+    public static void main(String[] args) {
+        
+    }
+    public static void name() {
+        
+    }
+}
