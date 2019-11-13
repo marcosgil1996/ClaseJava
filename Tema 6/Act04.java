82ce073113b2d247cac7b5d9d3b2ea4fe1840372
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-13 16:39:02 +0100

    Update Act04.java

--- Tema 6/Act04.java
+++ Tema 6/Act04.java
@@ -1,8 +1,13 @@
 /**
  * Act4 Escribe un método que devuelva el mayor de cuatro números
  */
-public class Act04 {
 
-    public static void main(String[] args) {
-    //*  
-    }
+ /**
+  * Act04
+  */
+ public class Act04 {
+ 
+     public static void main(String[] args) {
+         
+     }
+ }
