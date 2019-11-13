cf88df6b0889870c6a542ad0ea40b8ee7438e75b
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-13 16:38:59 +0100

    Update Act10.java

--- Tema 6/Act10.java
+++ Tema 6/Act10.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
 /**
- * Act10
+ * Act10 Escribe un método que imprima la tabla de multiplicar de un número. El programa pediránúmeros enteros hasta que el usuario introduzca un número negativo
  */
 public class Act10 {
     static Scanner teclado = new Scanner(System.in);
     public static void main(String[] args) {
         int num;
-        System.out.println("\n Porfavor ingresa el numero de su tabla de multiplicar");
-        num = teclado.next();
-        teclado.nextLine();
-        tabla(i, n);   
 
+        do {System.out.println("\n Porfavor ingresa el numero de su tabla de multiplicar");
+            num = teclado.nextInt();
+            teclado.nextLine();
+        } while (num >0);
         
+        teclado.close();   
     }
     static void tabla(int i, int n){
         if (i <= 10) {
             System.out.print(i +"*"+ n + "=" + i*n +"\n");
             tabla(i+1, n);
-            return tabla;
         }
     }
 
-  
+
 }
