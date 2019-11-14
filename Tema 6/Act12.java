7b102eec413bc98fac3df967cdeb7d208c6c10ee
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-14 18:00:54 +0100

    Update Act12.java

--- Tema 6/Act12.java
+++ Tema 6/Act12.java
@@ -5,32 +5,43 @@ import java.util.Random;
 
 public class Act12 {
 
+    public enum Notas{
+        SOBRESALIENTE, NOTABLE, BIEN, SUFICIENTE, INSUFICIENTE, ERROR;
+    }
     public static void main(String[] args) {
         final int NOTA_FINAL = 51;
         System.out.println("=== Notas ===");
         getImpresion(NOTA_FINAL);
         System.out.println("=== === === ===");
-        
     }
-    public static float randomNumber(Float min, Float max){
+    /**
+     * 
+     * @param min
+     * @param max
+     * @return
+     */
+    public static float randomNumber(float min, float max){
         
         Random rnd = new Random(); //Invocamos la clase ramdom
         return min + rnd.nextFloat()*(max-min);
     }
+    /**
+     * 
+     * @param usuarios
+     */
     public static void getImpresion(int usuarios){
        
         int numeroAle = 5;
         String user = "Usuarios";
         
 
-        for(int i = 0; i < usuarios; i++){
+        for(float i = 0; i < usuarios; i++){
             numeroAle = randomNumber(0,10);
-            System.out.printf("%-5s%-2d%5d\n",user,i, numeroAle); 
+
+            System.out.printf("%-5s%-2d%5d\n",user,i, numeroAle, Notas); 
         }
     }
-
-    public static void getNotas(int Notas) {
+    static Calificaciones{
         
-    
     }
 }
