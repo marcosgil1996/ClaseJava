037655bda408096aefbd710a0f7a33d645461eec
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-20 18:03:10 +0100

    Create Act20b.java

---
+++ Tema 6/Act20b.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Act20b
+ */
+public class Act20b {
+    public static Scanner teclado;
+    public static void main(String[] args) {
+        teclado = new Scanner(System.in);
+        
+    }
+
+}
