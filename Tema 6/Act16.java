ce14352344de9ddd910b86108f037e887a149c81
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-20 13:08:52 +0100

    Update Act16.java

---
+++ Tema 6/Act16.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * Act16
+ */
+public class Act16 {
+
+    public static void main(String[] args) {
+        String numero;
+        String letra;
+        Scanner read = new Scanner(System.in);
+        System.out.println("Que numero quieres remplazar?: ");
+        numero = read.nextLine();
+        System.out.println("Que letra quieres remplazar?: ");
+        letra = read.nextLine();
+    }
+    public static void getNumeros(String numero, String letra) {
+        String si, sj, sk, sh ="";
+        for(int i=0){
+
+        }
+        
+    }
+}
