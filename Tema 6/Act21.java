0e9565d6fa5ab96cda1fb96d6b06f6b6dc8efe96
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-11-22 17:59:32 +0100

    Create Act21.java

---
+++ Tema 6/Act21.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Act21
+ */
+public class Act21 {
+
+    public static Scanner teclado;
+    public static Random rnd;
+   public static void main(String[] args) {
+       teclado = new Scanner(System.in);
+       boolean cicloJuego = true; 
+       System.out.println("=== BlackJack===");
+       do {
+           
+       } while (cicloJuego);
+
+   }
+
+   
+
+}
