b6b7233065445d2958e663d4c6cbc3c84114b24a
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-12-03 19:20:55 +0100

    tema7

---
+++ Tema 7/src/main/java/Act1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+/**
+ * Act1
+ */
+public class Act1 {
+
+    public Act1(){
+        String frase = "Hola caracola";
+        frase = primeraMayuscula(frase)
+    }
+
+    public String primeraMayuscula(String frase){
+
+    }
+}
---
+++ Tema 7/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Act1 ej1= new Act1();
+    }
+}
