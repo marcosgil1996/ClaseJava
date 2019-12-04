965e5d87786ea36697286cc6c958dc67a8c6867e
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-12-04 17:31:54 +0100

    Update Act02.java

--- Tema 7/src/main/java/Act02.java
+++ Tema 7/src/main/java/Act02.java
@@ -4,19 +4,22 @@ public class Act02{
 
     public Act02(){
         Scanner teclado = new Scanner(System.in);
+        int contadorVocal = 0;
+        int contadorConst = 0;
+
         String frase = "";
         System.out.println("*** Ingrese la frase ***");
         frase = teclado.nextLine();
+        System.out.println("Tienes" + getVocal + "Vocales" + "y" + );
 
         teclado.close();
     }
-
     public static boolean getVocal(char c) {
-        if ((Character.toLowerCase(c) == 'a') || (Character.toLowerCase(c) == 'e')|| (Character.toLowerCase(c) == 'i') || (Character.toLowerCase(c) == 'o') || (Character.toLowerCase(c) == 'u'))
+        if ((Character.toLowerCase(c) == 'a') || (Character.toLowerCase(c) == 'e')|| (Character.toLowerCase(c) == 'i')
+                || (Character.toLowerCase(c) == 'o') || (Character.toLowerCase(c) == 'u'))
             return true;
         else
             return false;
-        }
     }
 }
 
