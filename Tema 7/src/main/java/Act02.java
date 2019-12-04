81a8459d53d4ebe4c2724b959dca01ffb8a94898
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-12-04 17:52:59 +0100

    build

--- Tema 7/src/main/java/Act02.java
+++ Tema 7/src/main/java/Act02.java
@@ -1,25 +1,81 @@
 import java.util.Scanner;
-
 public class Act02{
 
-    public Act02(){
-        Scanner teclado = new Scanner(System.in);
-        int contadorVocal = 0;
-        int contadorConst = 0;
+    public static void getAct02(String[] args) {
+        // Metodo scanner para introducir datos desde la consola
+        Scanner in = new Scanner(System.in);
+        //Declaramos variables
+        String texto;
+        int contador1 = 0, contador2 = 0,consonantes,espacio = 0, guion = 0, punto = 0, coma = 0;
+        int vocal_a = 0;
+        int vocal_e = 0;
+        int vocal_i = 0;
+        int vocal_o = 0;
+        int vocal_u = 0;
 
-        String frase = "";
-        System.out.println("*** Ingrese la frase ***");
-        frase = teclado.nextLine();
-        System.out.println("Tienes" + getVocal + "Vocales" + "y" + );
+        System.out.println("introduzca un texto");
+        //metodo que obtiene los datos ingresados
+        texto = in.nextLine();
+        for (int i=0; i<texto.length(); i++){
+            contador1++;
+        }
+        for (int i=0; i < texto.length(); i++){
+            //Condición que evalua la cadena ingresada
+            if((texto.charAt(i)=='a')||(texto.charAt(i)=='e')||
+                    (texto.charAt(i)=='i')||(texto.charAt(i)=='o')||
+                    (texto.charAt(i)=='u')){
+                contador2++;
+            }
+            if(texto.charAt(i)==' '){
+                espacio++;
+            }
+            if(texto.charAt(i)=='-'){
+                guion++;
+            }
+            if(texto.charAt(i)=='.'){
+                punto++;
+            }
+            if(texto.charAt(i)==','){
+                coma++;
+            }
 
-        teclado.close();
-    }
-    public static boolean getVocal(char c) {
-        if ((Character.toLowerCase(c) == 'a') || (Character.toLowerCase(c) == 'e')|| (Character.toLowerCase(c) == 'i')
-                || (Character.toLowerCase(c) == 'o') || (Character.toLowerCase(c) == 'u'))
-            return true;
-        else
-            return false;
+            if(texto.charAt(i)=='a'){
+                vocal_a++;
+            }
+
+            if(texto.charAt(i)=='e'){
+                vocal_e++;
+            }
+
+            if(texto.charAt(i)=='i'){
+                vocal_i++;
+            }
+            if(texto.charAt(i)=='o'){
+                vocal_o++;
+            }
+            if(texto.charAt(i)=='u'){
+                vocal_u++;
+            }
+        }
+        //Resta de las variables contador
+
+        consonantes = contador1-contador2-espacio-guion-punto-coma;
+        System.out.println(contador1 + " Caracteres " );
+        System.out.println(contador2 + " Vocales " );
+        System.out.println(vocal_a + " a " );
+        System.out.println(vocal_e + " e " );
+        System.out.println(vocal_i + " i " );
+        System.out.println(vocal_o + " o " );
+        System.out.println(vocal_u + " u " );
+        System.out.println(consonantes + " Consonantes " );
+        System.out.println(espacio + " Espacios " );
+        System.out.println(guion + " Guiones " );
+        System.out.println(punto + " Puntos " );
+        System.out.println(coma + " Comas " );
+
     }
 }
+
+
+
 
