c96d14da54715a4d2198584e698645bf5f03af5d
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-12-10 17:47:39 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Tema 7/src/main/java/Act02.java

--- Tema 7/src/main/java/Main.java
+++ Tema 7/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Act1 ej1= new Act1();
+        //Act01 ej1= new Act01();
         //Act02 ej2 = new Act02();
         //Act03 eje2b = new Act03();
         //Act04 eje4b = new Act04();
