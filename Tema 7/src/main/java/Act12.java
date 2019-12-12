d55a73c0fb82d967ba83816cb7d3f96c8f778772
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2019-12-12 12:50:24 +0100

    Finalizado

--- Tema 7/src/main/java/Act12.java
+++ Tema 7/src/main/java/Act12.java
@@ -2,27 +2,33 @@ import java.util.Scanner;
 
 public class Act12 {
     public Act12(){
-        Scanner teclado = new Scanner(System.in);
-        String frase ="";
-        System.out.println("Introduce la frase");
-        frase = teclado.nextLine();
-        frase = frase.trim();
-        frase = frase.toLowerCase();
-        frase = Digits(frase);
-        frase  = Words(frase);
-        teclado.close();
+        Scanner teclado = new Scanner(System.in); // invocamos la clase escaner para poder leer los datos a los usuario
+        String frase =""; //un String vacio para guardar los datos que introduzcan los usuarios
+        System.out.println("Introduce la frase"); //Se le piden los datos a los usuarios
+        frase = teclado.nextLine(); //Se guardan los datos de la frase introducida por los usuarios
+        frase = frase.trim(); //Eliminamos los espacios del principio y del final de la frase escrita por el usuario
+        frase = frase.toLowerCase(); //Pasamos palabras en mayúsculas en minusculas de la cadena
+        frase = Digits(frase); //llamada al método de Digits de sustitución
+        frase  = Words(frase); //llamada al métood de Words de sustitición
+        teclado.close(); //cierre del tecldo
 
     }
 
     /**
      *
-     * @param sentece
-     * @return
+     * @param sentece para la palabra
+     * @return un remplazo de palabras
      */
     public static String Digits(String sentece){
         sentece = sentece.replace("\\d", "*");
         return sentece;
     }
+
+    /**
+     *
+     * @param sentence para la palabra
+     * @return remplazo de palabra de "es" por "nopor"
+     */
     public static  String Words(String sentence){
         sentence = sentence.replaceAll("es","nopor");
         return  sentence;
