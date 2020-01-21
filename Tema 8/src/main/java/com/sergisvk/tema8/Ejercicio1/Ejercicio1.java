9f8430a0a362b25e123453c793de981652177aaf
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2020-01-21 12:54:18 +0100

    Modificación del proyecto

--- Tema 8/src/main/java/com/sergisvk/tema8/Ejercicio1/Ejercicio1.java
+++ Tema 8/src/main/java/com/sergisvk/tema8/Ejercicio1/Ejercicio1.java
@@ -1,4 +1,16 @@
 package com.sergisvk.tema8.Ejercicio1;
 
 public class Ejercicio1 {
+
+    public Ejercicio1() {
+        Coche c1 = new Coche();
+        Coche c2 = new Coche("Seat Ibiza", "Azul", true, "0123 KVD", Coche.TipoCoche.UTILITARIO, 2019, Coche.ModalidadSeguro.TERCEROS);
+        c1.imprimirCoche();
+        c2.imprimirCoche();
+        c1.setModelo("Peugeot 208");
+        c1.setAnyo(2015);
+        c1.imprimirCoche();
+        c2.setColor("Verde");
+        c2.imprimirCoche();
+    }
 }
