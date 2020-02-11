479167200c73d31e9ae8547992c2faea1a7d9f17
Sergi SvK <45337589+SergiSvK@users.noreply.github.com>
2020-02-11 19:10:47 +0100

    Revisión del proyecto

---
+++ Tema 9/src/main/java/com/sergisvk/tema9/Ejercicio5/Ejercicio5.java
@@ -0,0 +1,14 @@
+package com.sergisvk.tema9.Ejercicio5;
+
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+
+    public Scanner teclado;
+    public Ejercicio5(){
+
+    }
+
+
+}
--- Tema 9/src/main/java/com/sergisvk/tema9/Main.java
+++ Tema 9/src/main/java/com/sergisvk/tema9/Main.java
@@ -1,12 +1,14 @@
 package com.sergisvk.tema9;
 
+import com.sergisvk.tema9.Ejercicio5.Ejercicio5;
 import com.sergisvk.tema9.ejercicio1.Ejercicio1;
 import com.sergisvk.tema9.ejercicio2.Ejercicio2;
 
 public class Main {
     public static void main(String[] args) {
         //Ejercicio1 ejer1 = new Ejercicio1();
-        Ejercicio2 ejer2 = new Ejercicio2();
+        //Ejercicio2 ejer2 = new Ejercicio2();
+        Ejercicio5 ejer3 = new Ejercicio5();
 
     }
 
